a9c6f64be80e189862929959bc14ca979772372b
Sergio Rodriguez Seoane <sergio.rodriguez.seoane@udc.es>
2022-09-11 00:08:39 +0200

    Cambios bspwm autostart y polybar volume

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -47,11 +47,11 @@ xsetroot -cursor_name left_ptr &
 run nm-applet &
 run pamac-tray &
 #run xfce4-power-manager &
-numlockx on &
+numlockx off &
 blueberry-tray &
 picom --config $HOME/.config/picom/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/xfce4/notifyd/xfce4-notifyd &
+#/usr/lib/xfce4/notifyd/xfce4-notifyd &
 run volumeicon &
 #nitrogen --restore &
 #run caffeine &
